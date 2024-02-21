20a1b52aac6ac3260e7049ca50bcf8e6f41b52a6
David Keller <david.keller@litchis.fr>
2024-02-21 23:39:09 +0100

    Add serialization library
    Signed-off-by: David Keller <david.keller@litchis.fr>

---
+++ ports/ks-serialization/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_from_git(
+	OUT_SOURCE_PATH SOURCE_PATH
+	URL git@github.com:Keepsake/serialization.git
+	REF c5805a87d14ec90fbfddee41de044b6a7b677268
+	HEAD_REF develop
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH
+    "${SOURCE_PATH}"
+  OPTIONS
+    -DBUILD_TESTING=OFF
+    -DBUILD_BENCHMARK=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME KsSerialization)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(
+  INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
