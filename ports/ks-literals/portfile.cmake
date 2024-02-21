f41ebffceb21499a008e4f4ea93594e6a7327247
David Keller <david.keller@litchis.fr>
2024-02-21 23:54:07 +0100

    Add literals
    Signed-off-by: David Keller <david.keller@litchis.fr>

---
+++ ports/ks-literals/portfile.cmake
@@ -0,0 +1,25 @@
+vcpkg_from_git(
+	OUT_SOURCE_PATH SOURCE_PATH
+	URL git@github.com:Keepsake/literals.git
+	REF 5d767974c0d4f7b3be6bc84a41b9bc044e3f1fd4
+	HEAD_REF develop
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH
+    "${SOURCE_PATH}"
+  OPTIONS
+    -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME KsLiterals)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(
+  INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
