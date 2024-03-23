9b42ba1d140c226b56697a37c08dc4eecbb77164
David Keller <david.keller@litchis.fr>
2024-03-23 11:54:08 +0100

    Upgrade fatal
    Signed-off-by: David Keller <david.keller@litchis.fr>

--- ports/ks-fatal/portfile.cmake
+++ ports/ks-fatal/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO Keepsake/fatal
-	REF ebca43a9b2347f84ef44987f8a9ce0cb88d6bd6b
-  SHA512 f8c1825b6b8a7b08132881946cf4db0f8c3001428b15c6daf90a3ededeba4870f1aebe7cadb2ace38334c74fcb7d2523a2b3a5ff1ac401b09ad68133f846569c
-	HEAD_REF develop
+	REF 08519a92444838535b8e241fbb4d4bf5c33229f5
+  SHA512 cf3bb871c88520ec62385f5cfeee98ad985086730446a9ea6c17d998ef684bf6195c93bafc364def0a0f965ff10351577fcedbd007bf7d629e5ea7ae7aafc6fd
+	HEAD_REF unstable
 )
 
 vcpkg_cmake_configure(
