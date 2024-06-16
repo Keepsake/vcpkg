512348fcf4d371b1e1b26ebf5af13e6b9cbbcdaf
David Keller <david.keller@litchis.fr>
2024-06-16 23:26:45 +0200

    Update serialization library
    Signed-off-by: David Keller <david.keller@litchis.fr>

--- ports/ks-serialization/portfile.cmake
+++ ports/ks-serialization/portfile.cmake
@@ -1,8 +1,9 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO Keepsake/serialization
-	REF c5805a87d14ec90fbfddee41de044b6a7b677268
-	HEAD_REF develop
+	REF beba9676bbf15cf0d32dbaf88f71103df57e8203
+  SHA512 c93ebad869ad87542c4d9c3228a3a3599ef57161b39143c28c51633d7fc21b3d2fda1ce288a3e1c9e6523a2aea3ad955ec4b6b66443dcef37185b28cd0313c4a
+	HEAD_REF unstable
 )
 
 vcpkg_cmake_configure(
